b47a5d0277e26792d8cfb07eef961dd1c5a008a0
alexwestby11 <amdaq1@gmail.com>
2018-10-20 14:56:11 -0500

    Delete productPage.js

--- productPage.js
+++
@@ -1,8 +0,0 @@
-function getMap()
-{
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYW1kYXExIiwiYSI6ImNqbmhucnEybTBmdDQza216czR1eXZ2Y20ifQ.Ev-VQ1mnNpwDOgPV3gRTOA';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/amdaq1/cjnhq1uui1ea92rp462ng3gf9'
-    });
-}
