b801c8f40fc9bcdf4e87555d8564f9e6afed1253
alexwestby <alexwestby@tamu.edu>
2018-11-03 12:57:04 -0500

    Added Graph to represent recommendations

--- JS/productPage.js
+++ JS/productPage.js
@@ -1,6 +1,6 @@
 function createMap()
 {
-    mapboxgl.accessToken = "";
+    mapboxgl.accessToken = "pk.eyJ1IjoiYW1kYXExIiwiYSI6ImNqbmhucnEybTBmdDQza216czR1eXZ2Y20ifQ.Ev-VQ1mnNpwDOgPV3gRTOA";
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/amdaq1/cjnhq1uui1ea92rp462ng3gf9'
@@ -24,13 +24,19 @@ function createCarousel() {
     });
 }
 function createGraph(){
+
+       //Stands for current node being created
+       var currentNode= 6;
+       var childNodes=[];
+       var emptyArray= [];
+
         // create an array with nodes
         var nodes = new vis.DataSet([
-            {id: 1, label: 'Mouse'},
-            {id: 2, label: 'Macbook Pro'},
-            {id: 3, label: 'Mousepad'},
+            {id: 1, label: 'Mouse',isSelected: false},
+            {id: 2, label: 'Macbook Pro' },
+            {id: 3, label: 'Mousepad'   },
             {id: 4, label: 'Second Monitor'},
-            {id: 5, label: 'Laptop Sleeve'}
+            {id: 5, label: 'Laptop Sleeve' }
         ]);
 
         // create an array with edges
@@ -49,9 +55,72 @@ function createGraph(){
             nodes: nodes,
             edges: edges
         };
-        var options = {};
+        var options = {
+
+            nodes: {
+                color: {
+                    border: 'rgb(223,219,212)',
+                    background: 'rgb(223,219,212)',
+                    highlight: {
+                        border: '#2B7CE9',
+                        background: '#D2E5FF'
+                                }
+                        },
+                shape:'dot'
+                    },
+            edges:{
+                color: {
+                    color:'#848484',
+                    highlight:'#848484',
+                    hover: '#848484',
+                    inherit: 'from',
+                    opacity:1.0
+                },
+                width: 0.5
+            },
+            physics:{
+                enabled: true,
+                repulsion: {
+                    centralGravity: 0.8,
+                    springLength: 50,
+                    springConstant: 0.5,
+                    nodeDistance: 100,
+                    damping: 0.55
+                }
+            }
+
+        };
 
         // initialize your network!
         var network = new vis.Network(container, data, options);
+
+    network.on("selectNode", function(params) {
+        if (params.nodes.length > 0) {
+            childNodes=emptyArray;
+            var nodeId = params.nodes[0];
+            console.log("Clicked on a NODE with id = " + nodeId + ", label = " +
+                nodes.get(nodeId).label);
+            for(var i=6; i<=9; ++i)
+            {
+                addNodes(nodes,edges,currentNode,nodeId);
+                childNodes.push(currentNode);
+                currentNode++;
+            }
+        }
+    });
+
+
+
     }
 
+function addNodes(nodes,edges,givenNode,fromNode) {
+    nodes.add(  {id: givenNode, label: 'Mouse'})
+    edges.add(  {from: fromNode, to: givenNode})
+}
+function deleteNodes(nodes,edges,parentNode,childreNodes)
+{
+    for(var i=0; i<=childrenNodes.length-1;++i)
+    {
+
+    }
+}
