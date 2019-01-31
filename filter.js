2f4dda9c100a499de400d2dfac7865068e068338
alexwestby11 <amdaq1@gmail.com>
2019-01-31 12:36:11 -0600

    worked on json

--- filter.js
+++ filter.js
@@ -1,35 +1,34 @@
 function saveCategory(){
 var jsonObj = {
-"Category":{
-    "Camera":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Camera Lens":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Charger/Adapter":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Controller":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Desktop PC":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Docking Station":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "DVD/BluRay Players":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "External Hard Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "External Solid State Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Game":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Gaming Console":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "HDMI Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Headset":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Keyboard":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Laptop Charger":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Monitor":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Mouse":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Notebook":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Noteboook Case":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Printer":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "SD Card":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Smartphone":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Speaker":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "Tablet":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "TV":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "USB Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]},
-    "USB Flash Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"AvgPriceArray":[],"Box":[]}
+"Type":{
+    "Camera":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Camera Lens":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Charger/Adapter":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Controller":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Desktop PC":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Docking Station":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "DVD/BluRay Players":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "External Hard Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "External Solid State Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Game":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Gaming Console":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "HDMI Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Headset":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Keyboard":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Laptop Charger":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Monitor":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Mouse":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Notebook":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Noteboook Case":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Printer":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "SD Card":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Smartphone":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Speaker":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "Tablet":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "TV":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "USB Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+    "USB Flash Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]}
     }
-
 }
 
 localStorage['prices']=JSON.stringify(jsonObj)
@@ -72,47 +71,23 @@ function oneSigmaMinus(arrayGiven)
 
 
 
+
 function updateAveragePrice(x){
-    if(isNaN(localStorage['currentCollect'])){
-        localStorage['averagePrice'] = 0;
 
-        //avgPriceArray = (JSON.parse(localStorage.getItem("avgPriceArray"))).map(Number);
-        avgPriceArray.push(x)
-        localStorage.setItem("avgPriceArray", JSON.stringify(avgPriceArray));
-        var argo= (JSON.parse(localStorage.getItem("avgPriceArray"))).map(Number);
-        averagePrice = x;
-        localStorage['averagePrice'] = averagePrice;
-        localStorage['currentCollect'] = 0;
-        currentCollect =  Number(localStorage['currentCollect']);
-        ++currentCollect;
-        localStorage['currentCollect'] = currentCollect;
-        //alert('average price:'+ localStorage['averagePrice'])
-    }
-    else{
-        avgPriceArray = (JSON.parse(localStorage.getItem("avgPriceArray"))).map(Number);
-        avgPriceArray.push(x)
-        localStorage.setItem("avgPriceArray", JSON.stringify(avgPriceArray));
-        averagePrice = Number(localStorage['averagePrice']);
-        averagePrice += x;
-        localStorage['averagePrice'] = averagePrice;
-        currentCollect =  Number(localStorage['currentCollect']);
-        ++currentCollect;
-        localStorage['currentCollect'] = currentCollect;
-       // alert('average price:'+ localStorage['averagePrice'])
 
-    }
+        var typeString = localStorage['ID_type']
+        var typeJSON = (JSON.parse(localStorage['prices']));
+        typeJSON['Type'][typeString]['AvgPriceArray'].push(x);
+        typeJSON['Type'][typeString]['Count']=typeJSON['Type'][typeString]['Count']+1;
+        localStorage.setItem('prices',JSON.stringify(typeJSON))
 
-    if(currentCollect  >= totalCollect) {
+        if(currentCollect  >= totalCollect) {
 
-        avgPriceArray = (JSON.parse(localStorage.getItem("avgPriceArray"))).map(Number);
         lowerBoundPrice=oneSigmaMinus(avgPriceArray)
         if(lowerBoundPrice<0)
         {
             lowerBoundPrice=0
         }
         upperBoundPrice=oneSigmaPlus(avgPriceArray)
-       // localStorage['currentCollect'] = "NaN";
-        //alert('average price:'+ averagePrice/totalCollect)
-        //alert("average = " + averagePrice/totalCollect);
     }// alert(localStorage['currentCollect']);
-}
+    }
