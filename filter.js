0a399afa35d74fb09c5f138d09aa64965bfd702e
alexwestby11 <amdaq1@gmail.com>
2019-02-11 11:13:29 -0600

    Adding Brands functionality

--- filter.js
+++ filter.js
@@ -1,33 +1,192 @@
 var isFirst=true
+var brands=
+    [
+        'HP','Dell','Asus','Acer','Apple',
+        'Huawei','Microsoft','Lenovo','GIGABYTE','LG',
+        'Smart-US','JUMPER EZbook','XIDU','Samsung','Latitude',
+        'HP-Stream-Pro','Computer Upgrade King','HP-Stream-14-4GB-32GB','Samsung Chromebook','NUVISION',
+        'RCA','MSI','Oemgenuine','jumper','Sumsung',
+        'Google','BenQ','ViewSonic','Sceptre','AOC',
+        'Raspberry Pi','Planar','Viotek','Loncevon','Philips Computer Monitors',
+        'ELECROW','HP-monitors','Gechic','Kuman','Devmlicor',
+        'egiant','ProCase','MOSISO','JETech','AmazonBasics',
+        'CaseBuy','beyle','Lacdo','OneOdio','tomtoc',
+        'Qishare','NIDOO','Ytonet','LONMEN','KSW KINGDO',
+        'Thule','Mancro','amCase','changel','iCozzier',
+        'AboveTEK','FreeBiz','KEC','Cyber Acoustics','RAINYEAR make life easier',
+        'ARMYCAMOUSA','Govee','WIWU','Youpeck','BTSKY',
+        'kayond','KK Crafts','RUBAN','null','Naukay',
+        'CAISON','HOMIEE','YIDA','Comfyable','Great Wall',
+        'Ramhorn','Evecase','CCPK','BRINCH','Targus',
+        'VicTsing','Jelly Comb','PICTEK','Logitech','Havit',
+        'TECKNET','Tecland','LOVRI','PASONOMI','Redragon',
+        'USTEK','Catcha','Generic','inphic','LENRUE',
+        'Corsair','Chuanyue','Sonfan','by','SteelSeries',
+        'vive comb','Razer','Xergur','MChoice','Splaks',
+        'FIRSTBLOOD ONLY GAME.','FD','Victor','Hasbro','NR',
+        'VersionTECH','noximi','Rii','Philips','Anker',
+        'ARyee','BATURU','ZOZO','Bestland','Nicpower',
+        'LVSun','ELECJET','Superer','NEW POW','EBK',
+        'Ineedup','DTK','Powseed','TNP Products','Dell Charger',
+        'Power Depot','Reparo','Novelty','Selectec','ANTIEE',
+        'Westronix','PWR+','Belker','Outtag','Purpleleaf',
+        'SoulBay','ROLADA','Easy Style','Padarsey','Reo',
+        'ISUER','CHOETECH','Crucial','Born for Mac Notebook','VUOHOEG',
+        'Laimate','Novobit','aceyoon','PUIZHAK','SKstyle',
+        'DJW','HYTED','WEGWANG','Futurebatt','Tonb Shop',
+        'EBOYEE','MAGIPEA','Chicony','CHANZON','Toshiba',
+        'BIGPRIN INC','Charger','Acbel','PowerSource','SKY BOY',
+        'usmart','BAJ','Sopito','StarTech','CYD',
+        'Innergie','GANA','PEGLY','DODAUG','ChargerBuy',
+        'LeapFrog','TSKYBEAR','S SKstyle','Arteck','YEKBEE',
+        'Ombar','Azio','Das Keyboard','AUKEY','TOMOKO',
+        'TEC.BEAN','Kensington','Rottay','OfficeTec','Gamers Digital',
+        'LESHP','Macally','KLIM Technologies','Airfox','Mpow',
+        'Adesso','CHONCHOW','Rosewill','BFRIENDit','Zoweetek',
+        'OMOTON','Belkin','i-rocks','BronaGrand','EagleTec',
+        'ACEPHA','seenda','Aerb','iClever','TECWARE',
+        'Inland','HyperX','KHOMO','SecurOMax','Twisted Veins',
+        'HUANUO','BlueRigger','KabelDirekt','BAM','PowerBear',
+        'Fusion4k','Syncwire','Rankie','CableVantage','IBRAÃ‚',
+        'Signstek','polestar connect','C&E','Lesozoh','Sansui',
+        'Cable Matters','Farstrider','Sailfish','Monster','DIRECTV',
+        'NewBEP','Aurum Cables','MINC Cable','MPI','VAlinks',
+        'Kawany','Smart Stereo','Bugubird','Postta','Jumbl',
+        'Importer520','Maximm','Electop','Benfei','Monoprice',
+        'BATIGE','uni','Tuscom','BUSUQ','Capshi',
+        'RELM','UPTab','Letter Love Fashion Furniture','FosPower','Northbear',
+        'IBRAÂ®','PERLESMITH','AMICCOM','Cables Unlimited','ATEVON',
+        'Ultra Clarity Cables','iBirdie','Plugable','WAVLINK','Pezin & Hulin',
+        'BESTHING','Sabrent','Diamond Multimedia','zeateck','segrewall',
+        'EasyAcc','Rostmary GIFT Shop','Greta Oto Design','IOGEAR','Thermaltake',
+        'Sigma Sport','SIIG','Hyper','L.win','Sisun',
+        'Cshidworld','Elite Tech Gear','Asustek','Unitek','Poweroni',
+        'Pillarr','KEEPOW','ATZEBE','CableCreation','Hercules Tuff',
+        'Xiemin','Mediasonic','A ADAM ELEMENTS','Simicore','WEme',
+        'yinboti','Henge Docks','AudioSnax','BarvA','COSOOS',
+        'TOBENONE','MagiDeal','Zettaguard','KABCON','Amazon',
+        'Pyle','Cambridge Soundworks','WZK','DOSS','ZoeeTree',
+        'Vomach','SHAVA','Marvotek','Harman Kardon','OSD Audio',
+        'RECCAZR','JBL','Sylvania','AOMAIS','Zosam',
+        'APIE','COMISO','XLeader','Aduro','Rockville',
+        'Aptoyu','Ultimate Ears','VOCH','ZENBRE','Nyne',
+        'Klipsch','NUBWO','Long Run Technologies','WEYANG','Etekcity',
+        'Ozzie','Forcovr','SplashSound','JEFAL','KOOINGYUE',
+        'Soundcore','MIABOO','Sony','JONTER','Tribit',
+        'Xeneo','GOgroove','Soundbot','ARCHEER','NANYI',
+        'BAXIA TECHNOLOGY','Axess','NSOSAUE','Ematic','iGearPro',
+        'NEWBEING','Muzili','Vomercy','Lexon','N&M Tech',
+        'SOUNDBOKS','Paradigm','Ancord','Aneerx','Marshall',
+        'Elemusi','punkcase','Lonchan','Aiwa','Vtin',
+        'MIFA','BUMMD','AUCEE','Ewa','TaoTronics',
+        'Otis & Eleanor','Sbode','CT Sounds','Soundvox','Mix Hero',
+        'AURTEC','Bose','JSAUX','Coolwind','Auto Tech',
+        'dodocool','Gideon','SoundSOUL','I-STAR','Treblab',
+        'Sound Appeal','VOGUISH','GGMM','MiNE','DEWALT',
+        'Audioengine','Tronsmart','Polk Audio','HROMEN','UD',
+        'AYL','Photive','YELLYOUTH','Batai','Haehne',
+        'Victbing','Dragon Touch','Tagital','Azpen','Lectrus',
+        'ibowin','Yuntab','YDZB','Wecool','BENEVE',
+        'Aoson','Kivors','ALLDOCUBE','LLLCCORP','TOPELOTEK',
+        'B.B.PAW','Aurorax','CHUWI','llltrade','iRULU',
+        'Yue Ying','Simbans','reMarkable','Fusion5','Hoozo',
+        'TenYiDe','Contixo','NeuTab','Utopia Home','Nextbook',
+        'Winsing','Nokia','CUBOT','Unihertz','BLU',
+        'Motorola','Blackview','TracFone','UMIDIGI','Nokia mobile',
+        'RugGear','Kyocera','Xiaomi','Orchideous','Boost Mobile',
+        'Double-Lin','OnePlus','Xgody','AGM','Moto',
+        'NUU','Letv','Hipipooo','HTC','DORLAND',
+        'E&L','LG Electronics','Ulefone','Cubot KingKong','KEN XIN DA',
+        'DOOGEE','US-PopTrading','Samber','BlackBerry','Padcod',
+        'Sudroid','POPTEL','Kingston','JLXMROSE','LAXTEK',
+        'SanDisk','Best for most','PNY','AUAMOZ','Lexar',
+        'SP Silicon Power','Patriot','ZBHTCM','Everything But Stromboli','GGenerici',
+        'EBS LLC','BOYMXU','Polaroid','Transcend','Kingflash',
+        'Nasceret','Tomameri','ProGrade Digital Incorporated','FATTYDOVE','MiniMicro',
+        'Amplim','Beeway','Wolkano','TAMSOI','shenzs',
+        'Gigastone','HBJH','Queenwing','XINHUAYI','ADATA',
+        'Gleewin','AEGO','Verbatim','MYMAHDI','Panasonic',
+        'Vanja','Eco-Fused','fushen','Season','SOREDO',
+        'Canon','Brother','Epson','Kodak','Xerox',
+        'Lifeprint','Fujifilm','DYMO','HeroFiber','Dynastar',
+        'Tyler','OPPO Digital','Craig Electronics','Impecca','Marantz Professional',
+        'LONPOO','KASKAWISE','GPX','VocoPro','Pioneer',
+        'LP','AD ALICE DREAMS','PDP','PowerA','Total Control',
+        'Snakebyte','ThrustMaster','Collective Minds','Hori','KontrolFreek',
+        'NACON','RDS Industries','Qanba','ZTHY','BestPlayer',
+        'WANDESHA EASTE','BatPower','NiceTQ','RocketBus','BND',
+        'QIANXIANG','TPLTECH','JUYOON','Eton','VHBW',
+        'That Is Just Genius','IBERLS','Tripp Lite','SZYSK','JBtek',
+        'Factory Direct','Stone Digital','ANTOBLE','Smays','WEETEN',
+        'SOONHUA','ELENKER','PoderCamino','KINGDO','Ztotop',
+        'Ostrich','AC Doctor INC','iafer','Superadapter','YIPBOWPT',
+        'KJ-KayJI','Czznn','Atabletparts','YGJ','A-lig',
+        'Eaglewood','CANON(R)','Nikon','YISENCE','The Imaging World',
+        'AOM','AbergBest','Photo Savings','Olympus','GLOUE',
+        'Turtle Beach','HC GAMERLIFE','Sennheiser','Micolindun','RUNMUS',
+        'DLAND','REDESS','Jabra','ARKARTECH','Ceppekyy',
+        'ENVEL','BENGOO','BYZ','Amerzam','ONIKUMA',
+        'VersionTECH.','Sades','LaCie','Silicon Power','Western Digital',
+        'G-Technology','Apricorn','Seagate','KESU','Maxone',
+        'RAVPower','Intel','Oyen Digital','KOOTION','MyDigitalSSD',
+        'Glyph','KingDian','TEYADI','Titanium Micro','OWC',
+        'Nekteck','Blackjet','VectoTech','Blackmagic Design','Ubisoft',
+        'Sega','Warner Bros','Bandai Namco Entertainment America','2K Games','Konami',
+        'Capcom','Blizzard Entertainment','Activision','704 Games','Rockstar Games',
+        'Warner Home Video - Games','Square Enix','Koei','Sold Out','Electronic Arts',
+        'Bethesda','Studio Wildcard','Gearbox Publishing','Atlus','GAEMS',
+        'Atari','Timorn','Hermitshell','Sony.','Co2Crea',
+        'Enfain','iDiskk','HHAPPINESS','PACE','Picture Keeper',
+        'Yubico','VICFUN','JUANWE','JOIOT','RAOYI',
+        'Rhapsody & Mogan','Tecneca','Velcro Panel','Guchoi','BrexLink',
+        'SNANSHI','Pengo','UGREEN','SMALLElectric','Sigma',
+        'Rokinon','YONGNUO','Moment','Tamron','VIZIO',
+        'Jensen','PROSCAN','Toshiba TV','Hisense','ASA',
+        'Hitachi','TCL','Hitachi(r)'
+    ]
 var jsonObj = {
-"Type":{
-    "Camera":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Camera Lens":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Charger/Adapter":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Controller":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Desktop PC":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Docking Station":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "DVD/BluRay Players":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "External Hard Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "External Solid State Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Game":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Gaming Console":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "HDMI Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Headset":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Keyboard":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Laptop Charger":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Monitor":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Mouse":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Notebook":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Noteboook Case":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Printer":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "SD Card":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Smartphone":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Speaker":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "Tablet":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "TV":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "USB Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
-    "USB Flash Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]}
+    "Price":
+    [{
+        "Camera":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Camera Lens":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Charger/Adapter":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Controller":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Desktop PC":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Docking Station":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "DVD/BluRay Players":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "External Hard Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "External Solid State Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Game":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Gaming Console":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "HDMI Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Headset":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Keyboard":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Laptop Charger":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Monitor":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Mouse":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Notebook":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Noteboook Case":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Printer":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "SD Card":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Smartphone":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Speaker":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "Tablet":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "TV":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "USB Cable":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]},
+        "USB Flash Drive":{"StdDev":0,"Mean":0,"UpperBound":0,"LowerBound":0,"Count":0,"AvgPriceArray":[],"Box":[]}
+    }],
+    "Brand":
+    {
+
+
+    }
+}
+
+function addJSONParameter(givenJSONObj)
+{
+    for(i=0; i<=brands.length-1;++i)
+    {
+        givenJSONObj['Brand'][brands[i]]['Count']=0
     }
 }
 
@@ -73,29 +232,42 @@ function oneSigmaMinus(arrayGiven)
 
 
 
-function updateAveragePrice(x){
+function updateAveragePrice(x)
+{
 
         if(localStorage['prices']==null)
         {
             localStorage.setItem('prices',JSON.stringify(jsonObj))
             isFirst=false
         }
-        avgPriceArray
+
         var typeString = localStorage['ID_type']
         var typeJSON = (JSON.parse(localStorage['prices']));
-        typeJSON['Type'][typeString]['AvgPriceArray'].push(x);
-        typeJSON['Type'][typeString]['Count']=typeJSON['Type'][typeString]['Count']+1;
+        typeJSON['Price']=typeJSON['Price'].sort(function(a, b){return b.Count - a.Count})
 
+        typeJSON['Price'][typeString]['AvgPriceArray'].push(x);
+        var avgPriceArray=typeJSON['Price'][typeString]['AvgPriceArray'].map(Number)
+        typeJSON['Price'][typeString]['Count']=typeJSON['Price'][typeString]['Count']+1;
 
-
-        lowerBoundPrice=oneSigmaMinus(avgPriceArray)
+        var lowerBoundPrice=oneSigmaMinus(avgPriceArray)
         if(lowerBoundPrice<0)
         {
             lowerBoundPrice=0
         }
-        upperBoundPrice=oneSigmaPlus(avgPriceArray)
-        typeJSON['Type'][typeString]["UpperBound"]=upperBoundPrice
-        typeJSON['Type'][typeString]["LowerBound"]=lowerBoundPrice
+        var upperBoundPrice=oneSigmaPlus(avgPriceArray)
+        typeJSON['Price'][typeString]["UpperBound"]=upperBoundPrice
+        typeJSON['Price'][typeString]["LowerBound"]=lowerBoundPrice
         localStorage.setItem('prices',JSON.stringify(typeJSON))
-}// alert(localStorage['currentCollect']);
+}
+function updateBrand(brand)
+{
+
+    if(localStorage['prices']!=null)
+    {
+        var localJSON=JSON.parse(localStorage['prices'])
+        localJSON["Brand"][brand]["Count"]=localJSON["Brand"][brand]["Count"]+1
+    }
+
+
+}
 
